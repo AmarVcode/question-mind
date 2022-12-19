e91099a31c7b67d8c134e1693db1a3e0fb4216b0
@amarvcode <amarvcode@gmail.com>
2022-12-19 22:39:29 +0530

    completed

---
+++ main.js
@@ -0,0 +1,162 @@
+
+let q1=true
+
+
+function question1(n="1"){
+    if(q1===true){
+
+    
+if(n==="22/7"){
+    document.getElementById("active11").style.background="lime"
+    
+    let score=document.getElementById("score");
+    let s=score.innerText;
+    s=Number(s)
+    s=s+1000
+    score.innerText=s
+}
+else{
+document.getElementById("active11").style.background="red"
+}
+}
+
+q1=false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+let q2=true
+
+
+function question2(n="1"){
+    if(q2===true){
+
+    
+if(n==="16"){
+    document.getElementById("active12").style.background="lime"
+    
+    let score=document.getElementById("score");
+    let s=score.innerText;
+    s=Number(s)
+    s=s+1000
+    score.innerText=s
+}
+else{
+document.getElementById("active12").style.background="red"
+}
+}
+
+q2=false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let q3=true
+
+
+function question3(n="1"){
+    if(q3===true){
+
+    
+if(n==="3"){
+    document.getElementById("active13").style.background="lime"
+    
+    let score=document.getElementById("score");
+    let s=score.innerText;
+    s=Number(s)
+    s=s+1000
+    score.innerText=s
+}
+else{
+document.getElementById("active13").style.background="red"
+}
+}
+
+q3=false
+}
+
+
+
+
+
+
+
+
+
+
+let q4=true
+
+
+function question4(n="1"){
+    if(q4===true){
+
+    
+if(n==="28"){
+    document.getElementById("active14").style.background="lime"
+    
+    let score=document.getElementById("score");
+    let s=score.innerText;
+    s=Number(s)
+    s=s+1000
+    score.innerText=s
+}
+else{
+document.getElementById("active14").style.background="red"
+}
+}
+
+q4=false
+}
+
+
+
+
+
+
+
+
+
+
+let q5=true
+
+
+function question5(n="1"){
+    if(q5===true){
+
+    
+if(n==="sir"){
+    document.getElementById("active15").style.background="lime"
+    
+    let score=document.getElementById("score");
+    let s=score.innerText;
+    s=Number(s)
+    s=s+1000
+    score.innerText=s
+}
+else{
+document.getElementById("active15").style.background="red"
+}
+}
+
+q5=false
+}
